9b33966995923f8f9340b8b44343b0e3e477bbc1
titem-012 <titemmifta@gmail.com>
2023-09-24 15:30:33 -0700

    %

--- monty.c
+++ monty.c
@@ -1,81 +1,30 @@
-#ifndef MONTY_H
-#define MONTY_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <ctype.h>
-/**
-* struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
+#include "monty.h"
 
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO Holberton project
-*/
-typedef struct stack_s
-{
-int n;
-struct stack_s *prev;
-struct stack_s *next;
-}
-stack_t;
 /**
-* struct bus_s - variables -args, file, line content
-* @arg: value
-* @file: pointer to monty file
-* @content: line content
-* @lifi: flag change stack <-> queue
-* Description: carries values through the program
-*/
-typedef struct bus_s
+  * main - opens a monty script file for parsing.
+  * @argc: number of arguments.
+  * @argv: argument vector.
+  *
+  * Return: EXIT_SUCCESS on success or
+  * EXIT_FAILURE on failure.
+  */
+int main(int argc, char *argv[])
 {
-char *arg;
-FILE *file;
-char *content;
-int lifi;
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	parsefile(file);
+	return (EXIT_SUCCESS);
 }
-bus_t;
-extern bus_t bus;
-/**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
-*
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO Holberton project
-*/
-typedef struct instruction_s
-{
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
-}
-instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
-#endif
